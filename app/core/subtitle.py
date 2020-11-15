43b9a8d14a533cc130c49055dc8574af59b25672
sjquant <seonujang92@gmail.com>
2020-11-15 17:57:18 +0900

    Implement subtitle matcher

--- app/core/subtitle.py
+++ app/core/subtitle.py
@@ -1,15 +1,34 @@
 import abc
 import re
 
+import pandas as pd
 
-class BaseReader(abc.ABC):
+
+class SubtitleList(list):
+    def to_df(self):
+        return pd.DataFrame(self).set_index("time")
+
+    def to_csv(self, path: str, encoding="utf-8"):
+        self.to_df().to_csv(path, encoding=encoding)
+
+
+class BaseSubtitle(abc.ABC):
+    def __init__(self, text: str):
+        self._subtitles = self.read(text)
+
+    def __getitem__(self, idx: int):
+        return self._subtitles[idx]
+
+    def __len__(self):
+        return len(self._subtitles)
+
     @abc.abstractmethod
-    def read(self, file):
+    def read(self, text: str):
         pass
 
 
-class SRTReader(BaseReader):
-    def _trim(self, text):
+class SRTSubtitle(BaseSubtitle):
+    def _trim(self, text: str):
         text = text.replace("\r", "")
         # Remove html
         text = re.sub(r"<[^>]*>", "", text)
@@ -33,9 +52,9 @@ class SRTReader(BaseReader):
         seconds = int(h) * 3600 + int(m) * 60 + int(s)
         return seconds
 
-    def read(self, text):
+    def read(self, text: str):
         text = self._trim(text)
-        lines = []
+        lines = SubtitleList()
         re_sync = r"(\d\d:\d\d:\d\d),\d+\s*-->\s*\d\d:\d\d:\d\d,\d+\s*"
         iterator = re.finditer(re_sync, text, flags=re.IGNORECASE)
         prev_start = 0
@@ -50,13 +69,12 @@ class SRTReader(BaseReader):
                 prev_seconds = self._process_time(each.group(1))
                 continue
 
-            lines.append((prev_seconds, line))
+            lines.append({"time": prev_seconds, "line": line})
             prev_seconds = self._process_time(each.group(1))
-
         return lines
 
 
-class SMIReader(BaseReader):
+class SMISubtitle(BaseSubtitle):
     def _trim(self, text):
         text = text.replace("\r", "")
         extracted_text = re.search(
@@ -82,7 +100,7 @@ class SMIReader(BaseReader):
 
     def read(self, text):
         text = self._trim(text)
-        lines = []
+        lines = SubtitleList()
         re_sync = r"<SYNC Start[\s]*=[\s]*([0-9]+)[\s]*>"
         iterator = re.finditer(re_sync, text, flags=re.IGNORECASE)
         prev_start = 0
@@ -97,7 +115,47 @@ class SMIReader(BaseReader):
                 prev_seconds = self._process_time(each.group(1))
                 continue
 
-            lines.append((prev_seconds, line))
+            lines.append({"time": prev_seconds, "line": line})
             prev_seconds = self._process_time(each.group(1))
-
         return lines
+
+
+class SubtitleMatcher:
+    def __init__(self, subtitles: SubtitleList, translations: SubtitleList):
+        self._subtitles = subtitles
+        self._translations = translations
+
+    def _find_lte_idx(self, partition: SubtitleList, *, lte_time: int):
+        for idx, each in enumerate(partition):
+            if each["time"] > lte_time:
+                break
+        return idx
+
+    def _concat_translations(self, partition: SubtitleList):
+        translation = ""
+        for i, each in enumerate(partition):
+            if i != 0:
+                translation += " "
+            translation += each["line"]
+        return translation
+
+    def match(self):
+        prev_idx = 0
+        matched = SubtitleList()
+        for each in self._subtitles:
+            lte_idx = self._find_lte_idx(
+                self._translations[prev_idx:], lte_time=each["time"]
+            )
+            new_translation = self._concat_translations(
+                self._translations[prev_idx : prev_idx + lte_idx]  # noqa
+            )
+            matched.append(
+                {
+                    "time": each.pop("time"),
+                    "subtitle": each.pop("line"),
+                    "trnaslation": new_translation,
+                    **each,
+                }
+            )
+            prev_idx += lte_idx
+        return matched
--- tests/core/test_reader.py
+++ tests/core/test_reader.py
@@ -1,6 +1,6 @@
 import pytest
 
-from app.core.subtitle import SMIReader, SRTReader
+from app.core.subtitle import SMISubtitle, SRTSubtitle, SubtitleMatcher
 
 
 @pytest.fixture(scope="module")
@@ -18,26 +18,32 @@ def sample_srt():
 
 
 def test_read_smi(sample_smi):
-    reader = SMIReader()
-    data = reader.read(sample_smi)
+    subtitle = SMISubtitle(sample_smi)
 
-    time, line = data[5]
-    assert time == 50
-    assert line == "누군가를 보며 궁금해한 적 있나요"
+    data = subtitle[5]
+    assert data["time"] == 50
+    assert data["line"] == "누군가를 보며 궁금해한 적 있나요"
 
-    time, line = data[11]
-    assert time == 110
-    assert line == "라일리... - 너 좀 보렴"
+    data = subtitle[11]
+    assert data["time"] == 110
+    assert data["line"] == "라일리... - 너 좀 보렴"
 
 
 def test_read_srt(sample_srt):
-    reader = SRTReader()
-    data = reader.read(sample_srt)
+    subtitle = SRTSubtitle(sample_srt)
 
-    time, line = data[3]
-    assert time == 56
-    assert line == "Well, I know. Well, I know Riley's head."
+    data = subtitle[3]
+    assert data["time"] == 56
+    assert data["line"] == "Well, I know. Well, I know Riley's head."
 
-    time, line = data[8]
-    assert time == 114
-    assert line == "Aren't you a little bundle of joy?"
+    data = subtitle[8]
+    assert data["time"] == 114
+    assert data["line"] == "Aren't you a little bundle of joy?"
+
+
+def test_subtitle_matcher(sample_smi, sample_srt):
+    eng_subtitle = SRTSubtitle(sample_srt)
+    ko_subtitle = SMISubtitle(sample_smi)
+    matcher = SubtitleMatcher(eng_subtitle, ko_subtitle)
+    matched = matcher.match()
+    return matched
