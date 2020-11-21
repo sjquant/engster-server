e16586bb836df12077d12495214211a6ad9087a4
sjquant <seonujang92@gmail.com>
2020-11-21 22:02:35 +0900

    Add db utils

---
+++ app/utils/db.py
@@ -0,0 +1,17 @@
+async def fetch_all(query):
+    """
+    Fetch data from database in formatted form
+    """
+    data = await query.gino.all()
+    columns = [each.name for each in query.columns]
+    data = [dict(zip(columns, each)) for each in data]
+    return data
+
+
+async def fetch_one(query):
+    """
+    get data from database in formatted form
+    """
+    data = await query.gino.first()
+    data = {col.name: value for col, value in zip(query.columns, data)}
+    return data
