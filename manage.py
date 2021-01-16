61ded8c57849ccb127e596cc3cb443746ee617bd
sjquant <seonujang92@gmail.com>
2021-01-17 02:57:52 +0900

    Improve cli

--- manage.py
+++ manage.py
@@ -1,12 +1,14 @@
 from functools import wraps
-import json
 import asyncio
+import json
+import os
 
+from alembic.command import current, revision, history, upgrade, downgrade
 from alembic.config import Config
+import dotenv
 import click
 
-from app import config, create_app
-from app.services.user import create_user
+# Import app in each command functions to prevent config from prematurely being set
 
 
 def coroutine(f):
@@ -20,94 +22,84 @@ def coroutine(f):
 
 
 @click.group()
-def cli():
-    """
-    Simple CLI For Managing Sanic App
-    """
-    pass
+@click.option("--env-file")
+@click.pass_context
+def cli(ctx, env_file):
+    """Engster Command Line Interface"""
+    ctx.ensure_object(dict)
+
+    if env_file:
+        if not os.path.exists(env_file):
+            raise FileNotFoundError(f"{env_file} does not exists")
+        dotenv.load_dotenv(env_file)
+        print(f"Successfully Loaded {env_file}")
+
+    from app import config
+
+    alembic_ini_path = "./alembic.ini"
+    alembic_cfg = Config(alembic_ini_path)
+    alembic_cfg.set_main_option("db_url", config.DB_URL)
+    ctx.obj["alembic_cfg"] = alembic_cfg
 
 
 @cli.command(help="Show current revision")
-def current():
-    """
-    Show current revision
-    """
-    from alembic.command import current
-
-    alembic_ini_path = "./alembic.ini"
-    alembic_cfg = Config(alembic_ini_path)
-
+@click.pass_context
+def current_revision(ctx):
+    """Show current revision"""
+    alembic_cfg = ctx.obj["alembic_cfg"]
     current(alembic_cfg)
 
 
-@cli.command(help="Show history revision")
-def migrationshistory():
-    """
-    Show history revision
-    """
-    from alembic.command import history
-
-    alembic_ini_path = "./alembic.ini"
-    alembic_cfg = Config(alembic_ini_path)
-
+@cli.command()
+@click.pass_context
+def migrations_history(ctx):
+    """Show revision history"""
+    alembic_cfg = ctx.obj["alembic_cfg"]
     history(alembic_cfg)
 
 
-@cli.command(help="Auto make migrations")
-@click.option("-m", help="Migration message")
-def makemigrations(m):
-    """
-    Auto make migrations
-    """
-    from alembic.command import revision
-
-    alembic_ini_path = "./alembic.ini"
-    alembic_cfg = Config(alembic_ini_path)
-    alembic_cfg.set_main_option("db_url", config.DB_URL)
-
+@cli.command()
+@click.option("--mesagae", "-m", help="Migration message")
+@click.pass_context
+def make_migrations(ctx, message):
+    """Create migration file"""
+    alembic_cfg = ctx.obj["alembic_cfg"]
     revision_kwargs = {"autogenerate": True}
-    if m is not None:
-        revision_kwargs["message"] = m
+    if message is not None:
+        revision_kwargs["message"] = message
     revision(alembic_cfg, **revision_kwargs)
+    print("Successfully created migration files")
 
 
-@cli.command(help="Apply migrations")
-def migrate():
-    """
-    Apply migrations
-    """
-    from alembic.command import upgrade
-
-    alembic_ini_path = "./alembic.ini"
-    alembic_cfg = Config(alembic_ini_path)
-    alembic_cfg.set_main_option("db_url", config.DB_URL)
-
+@cli.command()
+@click.pass_context
+def migrate(ctx):
+    """Apply migrations to database"""
+    alembic_cfg = ctx.obj["alembic_cfg"]
     upgrade(alembic_cfg, "head")
+    print("Successfully upgraded migrations")
 
 
-@cli.command(help="Downgrade")
+@cli.command()
 @click.argument("revision", default="-1")
-def downgrade(revision):
-    """
-    Apply migrations
-    """
-    from alembic.command import downgrade
+@click.pass_context
+def downgrade_revision(ctx, revision):
+    """Downgrade revision"""
+    from app import config
 
-    alembic_ini_path = "./alembic.ini"
-    alembic_cfg = Config(alembic_ini_path)
+    alembic_cfg = ctx.obj["alembic_cfg"]
     alembic_cfg.set_main_option("db_url", config.DB_URL)
 
     downgrade(alembic_cfg, revision)
+    print("Successfully downgraded migrations")
 
 
-@cli.command(help="set genres and categories")
+@cli.command()
 @coroutine
-async def init():
-    """
-    initializing the project
-    :set categories and genres
-    """
-    from app import db
+@click.pass_context
+async def init_genres(ctx):
+    """Insert intial genres to database"""
+    from app import db, config
     from app.models import Genre
 
     await db.set_bind(config.DB_URL)
@@ -117,31 +109,37 @@ async def init():
 
     # Insert Genres
     await Genre.insert().gino.all(*genres)
+    print("Successfully inserted genres")
 
 
-@cli.command(help="runserver")
+@cli.command()
 def runserver():
+    """Run server"""
+    from app import create_app
+
     app = create_app()
     app.run(host="0.0.0.0", port=8000, debug=True)
 
 
-@cli.command(help="Create Admin User")
+@cli.command()
 @coroutine
 async def create_admin():
-    from app import db
+    """Create Admin User"""
+    from app import db, config
+    from app.services.user import create_user
 
     await db.set_bind(config.DB_URL)
 
-    email = input("Email : ")
-    nickname = input("Nickname : ")
-    password = input("Password : ")
-    password2 = input("Password Confirmation : ")
+    email = input("Email: ")
+    nickname = input("Nickname: ")
+    password = input("Password: ")
+    password2 = input("Password Confirmation: ")
 
     if password != password2:
         raise Exception("Password does not match!")
 
     await create_user(email=email, nickname=nickname, password=password, is_admin=True)
-    print(f"Successfully created admin : {nickname}")
+    print(f"Successfully created admin: {nickname}")
 
 
 if __name__ == "__main__":
